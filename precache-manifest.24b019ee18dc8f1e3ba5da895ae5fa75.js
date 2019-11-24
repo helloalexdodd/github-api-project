1b63a3c26ca13a3088014d10f42ea7d7b63374ef
helloalexdodd <alexdodd@gmx.com>
2019-11-24 18:02:58 -0500

    Updates

---
+++ precache-manifest.24b019ee18dc8f1e3ba5da895ae5fa75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c2c8f72724c5b20133aa5f0133def7",
+    "url": "/github-api-project/index.html"
+  },
+  {
+    "revision": "48c7fcd3ffd749b3f495",
+    "url": "/github-api-project/static/js/2.b8063773.chunk.js"
+  },
+  {
+    "revision": "c304f85a385dabe5ecd5",
+    "url": "/github-api-project/static/js/main.466251f2.chunk.js"
+  },
+  {
+    "revision": "e5f5203142c69f235c5f",
+    "url": "/github-api-project/static/js/runtime-main.86bda9f2.js"
+  },
+  {
+    "revision": "5d292e21532ea5857531cdb8b0180637",
+    "url": "/github-api-project/static/media/Octocat.5d292e21.jpg"
+  },
+  {
+    "revision": "f7bb33995187628002ddfef21f91564a",
+    "url": "/github-api-project/static/media/ShatteredOctocat.f7bb3399.png"
+  }
+]);
